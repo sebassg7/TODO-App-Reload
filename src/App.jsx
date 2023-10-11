399de85526273d86753d682c22bd40a55981a27e
Sebastian L Moran <sbssing7@gmail.com>
2023-10-10 19:28:31 -0500

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <TodoItem/>
+    </>
+  )
+};
+
+const TodoItem = () => {
+  return (
+    <li>
+      
+      <span>V</span>
+      <p>Headbanging con Metallica en la Antartica</p>
+      <span>X</span>
+
+    </li>
+  )
+};
+
+export default App

0e2d737418f9b7a2708c05540ebfa972681a0d90
Sebastian L Moran <sbssing7@gmail.com>
2023-10-11 11:21:30 -0500

    Eventos: onClick, onChange

--- src/CreateTodoButton.jsx
+++ src/CreateTodoButton.jsx
@@ -3,6 +3,16 @@ import './CreateTodoButton.css';
 
 export const CreateTodoButton = () => {
   return (
-    <button className='CreateTodoButton'>+</button>
+    <button 
+    className='CreateTodoButton'
+    onClick={(event) => {
+      console.log('Le diste clicl al boton')
+      console.log(event);
+      console.log(event.target);
+    }
+      
+    }
+    
+    >+</button>
   )
 }
--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -2,6 +2,13 @@ import './TodoSearch.css';
 
 export const TodoSearch = () => {
   return (
-    <input  placeholder="Agregar actividad" className="TodoSearch"/>
+    <input  
+    placeholder="Agregar actividad" 
+    className="TodoSearch"
+    onChange={(event) => {
+      console.log('Escribiste en el input')
+      console.log(event.target.value);
+    }}
+    />
   )
 }
